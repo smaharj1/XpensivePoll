6824b92d604489f8f63d05bc092c93e2c1259190
Sujil Maharjan <smaharj1@ramapo.edu>
2018-11-27 22:58:51 -0500

    Added icon package and made tabs

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import { StyleSheet , Text} from 'react-native';
-
+import Icon from "react-native-vector-icons/Ionicons";
 
 import Homepage from './src/components/Homepage';
 import Favorites from './src/components/Favorites';
 import Login from './src/components/Login';
 import { Router, Stack, Scene, Actions} from "react-native-router-flux";
 
-const TabIcon = ({ selected, title }) => {
+const TabIcon = ({ focused, title }) => {
+  let iconName = "ios-";
+  if(title == 'Favorites'){
+    iconName += "heart"
+  }
+  else if (title == 'Home'){
+    iconName += "home"
+  }
+  else {
+    iconName += "contact"
+  }
+
   return (
-    <Text style={{color: selected ? 'red' :'black'}}>{title}</Text>
+    <Icon
+      name={iconName}
+      size={25}
+      color={focused ? "#E89F2E" : "#333333"}
+    >
+
+    </Icon>
   );
 }
 
@@ -19,14 +36,15 @@ const App = () => {
       <Scene key="root" hideNavBar="true">
         <Scene 
           key="home"
-          tabs={true}
+          tabs
+          showLabel={false}
           tabBarStyle={{ backgroundColor: '#FFFFFF' }}
         >
           <Scene key="dashboard" component={Homepage} title="Home" initial icon={TabIcon}/>
           <Scene key="favorite" component={Favorites} title="Favorites" icon={TabIcon}/>
           
         </Scene>
-        <Scene key="login" component={Login} title="Login"/>
+        {/* <Scene key="login" component={Login} title="Login"/> */}
       </Scene>
     </Router>
   );
