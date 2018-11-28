31bf8d8d13f1bec318f5dad62ea28e01c151e5ba
Sujil Maharjan <smaharj1@ramapo.edu>
2018-11-27 22:33:44 -0500

    Created tabs

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet , Text} from 'react-native';
 
 
-import Homepage from './src/components/Homepage'
-import Login from './src/components/Login'
+import Homepage from './src/components/Homepage';
+import Favorites from './src/components/Favorites';
+import Login from './src/components/Login';
 import { Router, Stack, Scene, Actions} from "react-native-router-flux";
+
+const TabIcon = ({ selected, title }) => {
+  return (
+    <Text style={{color: selected ? 'red' :'black'}}>{title}</Text>
+  );
+}
 
 const App = () => {
   return (
     <Router>
-          <Stack key="root">
-            <Scene key="homepage" component={Homepage} title="Home"/>
-            <Scene key="login" component={Login} title="Login"/>
-          </Stack>
+      <Scene key="root" hideNavBar="true">
+        <Scene 
+          key="home"
+          tabs={true}
+          tabBarStyle={{ backgroundColor: '#FFFFFF' }}
+        >
+          <Scene key="dashboard" component={Homepage} title="Home" initial icon={TabIcon}/>
+          <Scene key="favorite" component={Favorites} title="Favorites" icon={TabIcon}/>
+          
+        </Scene>
+        <Scene key="login" component={Login} title="Login"/>
+      </Scene>
     </Router>
   );
 }
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,49 +1,46 @@
-import React from 'react';
-import { StyleSheet, Platform, Image, Text, View, ScrollView, TouchableOpacity } from 'react-native';
-import { GoogleSignin } from 'react-native-google-signin';
-import firebase from 'react-native-firebase';
+import React from "react";
+import {
+  StyleSheet,
+  Platform,
+  Image,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity
+} from "react-native";
+import { GoogleSignin } from "react-native-google-signin";
+import firebase from "react-native-firebase";
 
 export default class Homepage extends React.Component {
+  constructor() {
+    super();
+    this.autoSignIn = this.autoSignIn.bind(this);
+  }
 
-    constructor() {
-        super();
-        this.autoSignIn = this.autoSignIn.bind(this);
+  async componentDidMount() {
+    await this.autoSignIn();
+  }
+
+  async autoSignIn() {
+    try {
+      await GoogleSignin.configure();
+
+      const user = await GoogleSignin.signInSilently();
+      this.setState({ user });
+
+    } catch (error) {
+      // Navigate to login page.
+      Actions.login();
     }
+  }
 
-    async componentDidMount(){
-        await this.autoSignIn();
-    }
-
-    async autoSignIn(){
-        try {
-        await GoogleSignin.configure();
-
-        const user = await GoogleSignin.signInSilently();
-        this.setState({ user });
-
-        // Navigate to homepage.
-        } catch (error) {
-            console.warn(error.code)
-        // if (error.code === statusCodes.SIGN_IN_REQUIRED) {
-        //     // user has not signed in yet. Redirect to login page.
-        // } else {
-        // }
-
-        // Navigate to login page.
-        Actions.login();
-        }
-    }
-
-
-    render() {
-        return (
-            <ScrollView>
-                <View>
-                    <Text style={{fontSize: 30}}>Logged in Successfully!</Text>
-                </View>
-
-            </ScrollView>
-        )
-    }
-
+  render() {
+    return (
+      <ScrollView>
+        <View>
+          <Text style={{ fontSize: 30 }}>Logged in Successfully!</Text>
+        </View>
+      </ScrollView>
+    );
+  }
 }
